81c763e2185e35be8fec89fc0ed9dc497b2cf607
Mohamed Galis <mohamedg2903@gmail.com>
2025-03-27 09:21:46 +0000

    update

--- Simple Algebra Equation Test.py
+++ Simple Algebra Equation Test.py
@@ -4,9 +4,3 @@ print("Welcome To The Algebra Equation Test")
 name = input("Enter your name: ")
 print(f"Welcome, {name}! Let's test your algebra skills today.\n")
 print("Solve for x in each equation. You Got This! \n")
-
-Commit testing again
-
-Testing again
-
-test
